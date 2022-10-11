95fe1ec53aacd828388fa7b503fb2fa95f94bdd3
Parvej Alam <parvejaalam3502@gmail.com>
2022-10-11 08:38:55 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ProductsComponent } from './products/products.component';
+import { ProductComponent } from './product/product.component';
+import { ProductService } from './product.service';
+import { AddProductModelComponent } from './add-product-model/add-product-model.component';
+import { AddCustomerModelComponent } from './add-customer-model/add-customer-model.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductsComponent,
+    ProductComponent,
+    AddProductModelComponent,
+    AddCustomerModelComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+  ],
+  providers: [ProductService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
